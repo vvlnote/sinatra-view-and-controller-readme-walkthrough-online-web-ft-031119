d1af9764a07623b7f2dda8f3baabfd792e169b94
Joy Lin <vvlnote@hotmail.com>
2019-04-29 01:57:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,11 +6,12 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    puts params
     erb :reversed
   end
 
   get '/friends' do
     # Write your code here!
-
+    erb :friends
   end
 end
